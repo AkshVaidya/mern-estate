050726867355a74d35a3401aea748ae8942e8252
AkshVaidya <akshayvaidya2003@gmail.com>
2024-03-11 23:44:17 +0530

    install react js and tailwind css and create the firsst templte

---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    // Other PostCSS plugins if any
+  ],
+};
